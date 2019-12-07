25261bb9e12dd5229b74b8560aa02aa9efa1d4b0
yaogit666 <1397847010@qq.com>
2019-12-07 20:51:59 +0800

    12.7

---
+++ 奇数位上是奇数，偶数位上是偶数/奇数位上是奇数，偶数位上是偶数/源.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+	/**
+	 *  奇数位上都是奇数或者偶数位上都是偶数
+	 *  输入：数组arr，长度大于2
+	 *  len：arr的长度
+	 *  将arr调整成奇数位上都是奇数或者偶数位上都是偶数
+	 */
+	void oddInOddEvenInEven(vector<int>& arr, int len) {
+		int i = 0;
+		int j = 1;
+		while (i < len && j < len)
+		{
+			if (arr[len - 1] % 2 == 0)
+			{
+				swap(arr[len - 1], arr[i]);
+				i += 2;
+			}
+			else
+			{
+				swap(arr[len - 1], arr[j]);
+				j += 2;
+			}
+		}
+
+	}
+};
