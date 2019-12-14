6f7c73bf5138eec830cdb112fd6c2752b27b4131
yaogit666 <1397847010@qq.com>
2019-12-14 15:21:28 +0800

    12.14

---
+++ 机器人走格子/机器人走格子/源.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+	int uniquePaths(int m, int n) {
+		vector<vector<int>> v(m, vector<int>(n, 1));
+		if (m < 0 || n < 0)
+			return 0;
+		else
+		{
+			for (int i = 1; i < m; i++)
+			{
+				for (int j = 1; j < n; j++)
+				{
+					v[i][j] = v[i - 1][j] + v[i][j - 1];
+				}
+			}
+			return  v[m - 1][n - 1];
+		}
+	}
+};
