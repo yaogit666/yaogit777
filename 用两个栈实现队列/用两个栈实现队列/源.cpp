8b1becef22c52f82dccdc456997e2f72ff60121e
yaogit666 <1397847010@qq.com>
2019-12-28 17:41:28 +0800

    12.28

---
+++ 用两个栈实现队列/用两个栈实现队列/源.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+	void push(int node) {
+		stack1.push(node);
+	}
+
+	int pop() {
+		if (stack2.empty())
+		{
+			while (!stack1.empty())
+			{
+				int node = stack1.top();
+				stack1.pop();
+				stack2.push(node);
+			}
+		}
+		int a = stack2.top();
+		stack2.pop();
+		return a;
+	}
+
+private:
+	stack<int> stack1;
+	stack<int> stack2;
+};
