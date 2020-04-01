4d7df78e4dfce66ec3c8cac55cafb9fd4ddc6443
yaogit666 <1397847010@qq.com>
2020-04-01 21:00:36 +0800

    4.1

---
+++ 买卖股票的最佳时机/买卖股票的最佳时机/源.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+	int maxProfit(vector<int>& prices) {
+		if (prices.size() == 0)
+			return 0;
+		int len = prices.size();
+		vector<vector<int>> dp(len, vector<int>(2, 0));
+		dp[0][0] = 0;
+		dp[0][1] = -prices[0];
+		for (int i = 1; i < len; i++)
+		{
+			dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+			dp[i][1] = max(dp[i - 1][1], -prices[i]);
+		}
+		return dp[len - 1][0];
+	}
+};
