508dc054aa1cd9ddfcf6f7a678335a31efc120e0
yaogit666 <1397847010@qq.com>
2020-04-08 22:18:24 +0800

    4.8

---
+++ 删除排序数组中的重复项/删除排序数组中的重复项/源.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+	int removeDuplicates(vector<int>& nums) {
+		int len = nums.size();
+		if (len == 0)
+			return 0;
+		int i = 0;
+		int j = 0;
+		while (j < len)
+		{
+			if (nums[i] != nums[j])
+			{
+				nums[++i] = nums[j];
+			}
+			j++;
+		}
+		return i + 1;
+	}
+};
