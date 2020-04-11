ba1f61b43122a8a4922222aa020d04f9637779fb
yaogit666 <1397847010@qq.com>
2020-04-11 21:22:04 +0800

    4.11

---
+++ 链表相交/链表相交/源.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+		ListNode* pa = headA;
+		ListNode* pb = headB;
+		while (pa != pb)
+		{
+			if (pa == NULL)
+				pa = headB;
+			else
+				pa = pa->next;
+			if (pb == NULL)
+				pb = headA;
+			else
+				pb = pb->next;
+		}
+		return pa;
+	}
+};
