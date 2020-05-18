f489bb7ac4c67da0e5e8d1198b912a7488b42c1c
yaogit666 <1397847010@qq.com>
2020-05-18 17:39:33 +0800

    5.18

---
+++ 环路检测/环路检测/源.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode *detectCycle(ListNode *head) {
+		if (head == NULL || head->next == NULL)
+			return 0;
+		ListNode* fast = head;
+		ListNode* slow = head;
+
+		while (1)
+		{
+			fast = fast->next->next;
+			slow = slow->next;
+			if (fast == slow)
+				break;
+			if (fast == NULL || fast->next == NULL)
+				return 0;
+		}
+		fast = head;
+		while (fast != slow)
+		{
+			fast = fast->next;
+			slow = slow->next;
+		}
+		return slow;
+	}
+};
